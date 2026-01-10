ee4c3af32a179f8391ae4fdd1b4b69d30dea915b
Mateusz Fabisiak <mfabisiak@student.agh.edu.pl>
2026-01-11 00:06:39 +0100

    Refactor operators in Direction

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/Position.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/Position.kt
@@ -1,5 +1,6 @@
 package io.github.mfabisiak.darwinworld.model
 import io.github.mfabisiak.darwinworld.model.Direction.*
+
 data class Position(val x: Int, val y: Int) {
 
     operator fun plus(other: Position): Position = Position(this.x + other.x, this.y + other.y)
@@ -12,15 +13,15 @@ data class Position(val x: Int, val y: Int) {
 
 }
 
-fun Direction.toUnitVector(): Position = when(this){
-        NORTH -> Position(0,1)
-        NORTH_EAST -> Position(1,1)
-        EAST -> Position(1,0)
-        SOUTH_EAST -> Position(1,-1)
-        SOUTH -> Position(0,-1)
-        SOUTH_WEST -> Position(-1,-1)
+fun Direction.movement(): Position = when(this) {
+        NORTH -> Position(0, 1)
+        NORTH_EAST -> Position(1, 1)
+        EAST -> Position(1, 0)
+        SOUTH_EAST -> Position(1, -1)
+        SOUTH -> Position(0, -1)
+        SOUTH_WEST -> Position(-1, -1)
         WEST -> Position(-1,0)
-        NORTH_WEST -> Position(-1,1)
+        NORTH_WEST -> Position(-1, 1)
     }
 
 
