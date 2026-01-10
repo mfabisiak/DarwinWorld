1a75558cb047cc28e94f299108cd53093b2b21a4
Maciej Bednarczyk <mbednarczy@student.agh.edu.pl>
2026-01-10 03:09:24 +0100

    Add Animal class

---
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/Animal.kt
@@ -0,0 +1,23 @@
+package io.github.mfabisiak.darwinworld.model
+
+class Animal(var position: Position, initialEnergy: Int, val genotype: List<Int>, var direction: Direction){
+      val descendants = mutableSetOf<Animal>()
+      var energy: Int = initialEnergy
+        private set
+    val isAlive
+        get() = energy > 0
+
+    constructor(parent1: Animal, parent2: Animal) : this(
+        position=parent1.position,
+        genotype = generateGenotype(parent1, parent2),
+        initialEnergy = calculateEnergy(parent1, parent2),
+        direction = Direction.entries.random()
+
+    )
+    fun rotate(){
+
+    }
+
+
+
+}
