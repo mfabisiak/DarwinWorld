efe2552b777e01d1d1222de566f8ff6d9825e938
Maciej Bednarczyk <mbednarczy@student.agh.edu.pl>
2026-01-10 17:07:14 +0100

    Add overload operators to calculate directions

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/Direction.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/Direction.kt
@@ -1,13 +1,24 @@
 package io.github.mfabisiak.darwinworld.model
 
-enum class Direction {
-    NORTH,
-    NORTH_EAST,
-    EAST,
-    SOUTH_EAST,
-    SOUTH,
-    SOUTH_WEST,
-    WEST,
-    NORTH_WEST;
 
+enum class Direction(val index: Int) {
+    NORTH(0),
+    NORTH_EAST(1),
+    EAST(2),
+    SOUTH_EAST(3),
+    SOUTH(4),
+    SOUTH_WEST(5),
+    WEST(6),
+    NORTH_WEST(7);
+
+    operator fun plus(other:Int) : Direction = Direction.entries[(this.index+other)%8]
+
+    operator fun minus(other:Int) : Direction{
+        val directionAfterRotate = if (this.index-other>0){
+            (this.index-other)%8
+        } else {
+            (this.index-other+8)%8
+        }
+        return Direction.entries[directionAfterRotate]
+    }
 }
