9a03f5057b06f311899a3593e8aedd1f01453db7
Mateusz Fabisiak <mfabisiak@student.agh.edu.pl>
2026-01-11 04:17:34 +0100

    Add AnimalConfig integration and breeding logic to Animal class

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/animal/AnimalUtils.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/animal/AnimalUtils.kt
@@ -1,2 +1,34 @@
 package io.github.mfabisiak.darwinworld.model.animal
 
+import io.github.mfabisiak.darwinworld.model.Direction
+import io.github.mfabisiak.darwinworld.utils.AnimalConfig
+import kotlin.random.Random
+
+
+fun AnimalConfig.genotypeOfParents(parent1: Animal, parent2: Animal): Genotype {
+
+    val (leftParent, rightParent) = setOf(parent1, parent2).shuffled()
+
+    val divisionIndex = leftParent.energy / (leftParent.energy + rightParent.energy)
+
+    val mutatingGenes = GENE_RANGE.shuffled()
+        .take(Random.nextInt(minNumberOfMutations, maxNumberOfMutations - 1))
+
+    val genesList = listOf(
+        leftParent.genotype.genes.take(divisionIndex),
+        rightParent.genotype.genes.takeLast(genotypeSize - divisionIndex)
+    ).flatten().mapIndexed { index, gene ->
+        if (index in mutatingGenes) GENE_RANGE.random() else gene
+    }
+
+    return Genotype(genesList)
+
+}
+
+fun AnimalConfig.animalOfParents(parent1: Animal, parent2: Animal) = Animal(
+    config = this,
+    position = parent1.position,
+    genotype = genotypeOfParents(parent1, parent2),
+    initialEnergy = energyRequiredToBreed,
+    direction = Direction.random()
+)
