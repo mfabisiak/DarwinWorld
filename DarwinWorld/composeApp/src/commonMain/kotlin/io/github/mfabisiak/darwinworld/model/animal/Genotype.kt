aabd80d15915824f8de2eeeabcb780e35450f11b
Mateusz Fabisiak <mfabisiak@student.agh.edu.pl>
2026-01-11 02:31:17 +0100

    Refactor Genotype class to use CircularList for gene management

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/animal/Genotype.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/animal/Genotype.kt
@@ -1,34 +1,18 @@
 package io.github.mfabisiak.darwinworld.model.animal
 
-import kotlin.random.Random
+import io.github.mfabisiak.darwinworld.utils.CircularList
 
-class Genotype(private val genes: List<Int>) {
-    private var currentIndexOfGene= Random.Default.nextInt(genes.size)
+val GENE_RANGE = 0..7
 
-    fun nextRotation() : Int {
-        currentIndexOfGene+=1
-        return genes[currentIndexOfGene % genes.size]
-    }
-    companion object{
-        fun random(length: Int): Genotype = Genotype(List(length) { (0..7).random()})
+class Genotype(genesList: List<Int>) {
 
-        fun randomOfParents(parent1: Animal, parent2: Animal) : Genotype{
-            val totalEnergy = parent1.energy + parent2.energy
-            val divisionOfGenes = parent1.energy * parent1.genotype.genes.size / totalEnergy
-            val numberOfGenes = parent1.genotype.genes.size
+    val genes = CircularList(genesList)
 
-            val (leftGenesSource, rightGenesSource) = setOf(parent1.genotype, parent2.genotype).shuffled()
+    private val genesIterator = genes.randomStartingInfiniteIterator()
 
-            val shareParent1 = divisionOfGenes
-            val shareParent2 = parent1.genotype.genes.size - divisionOfGenes
-            val sideLeft = Random.Default.nextBoolean()
-            if (sideLeft){
-                return Genotype(parent1.genotype.genes.take(shareParent1) + parent2.genotype.genes.takeLast(shareParent2))
-            }else{
-                return Genotype( parent2.genotype.genes.take(shareParent2) + parent1.genotype.genes.takeLast(shareParent1))
-            }
-
-        }
-    }
-
+    fun nextGene() = genesIterator.next()
 }
+
+
+
+
