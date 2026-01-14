dd9868d9eea0b8e908cf7e783359b8d8064b8968
Maciej Bednarczyk <mbednarczy@student.agh.edu.pl>
2026-01-15 00:58:40 +0100

    Add AppConfig data model

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/App.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/App.kt
@@ -18,32 +18,16 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import darwinworld.composeapp.generated.resources.Res
 import darwinworld.composeapp.generated.resources.compose_multiplatform
+import io.github.mfabisiak.darwinworld.features.config.ConfigInput
+import io.github.mfabisiak.darwinworld.features.config.ConfigInputScreen
+
 
 @Composable
-@Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+    MaterialTheme{
+        ConfigInputScreen(
+            onSimulationStart = { config ->
+                println("$config")
+        })
     }
 }
