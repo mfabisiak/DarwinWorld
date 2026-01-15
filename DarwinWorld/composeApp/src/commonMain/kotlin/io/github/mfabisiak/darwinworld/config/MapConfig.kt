26110aa388dc631fb921bafd875e0769cd9198d7
Mateusz Fabisiak <mfabisiak@student.agh.edu.pl>
2026-01-15 18:45:55 +0100

    Refactor MapConfig and SimulationConfig interfaces to streamline inheritance from AnimalConfig

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/config/MapConfig.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/config/MapConfig.kt
@@ -3,7 +3,7 @@ package io.github.mfabisiak.darwinworld.config
 import io.github.mfabisiak.darwinworld.model.Position
 import io.github.mfabisiak.darwinworld.model.PositionClosedRange
 
-interface MapConfig {
+interface MapConfig : AnimalConfig {
     val lowerBound: Position
     val upperBound: Position
     val energyFromSinglePlant: Int
