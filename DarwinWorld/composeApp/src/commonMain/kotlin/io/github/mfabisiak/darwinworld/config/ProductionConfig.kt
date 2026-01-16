539945639b744f9d8151b592d1b38c9612e1be5d
Maciej Bednarczyk <mbednarczy@student.agh.edu.pl>
2026-01-16 01:20:48 +0100

    add default values in ProductionConfig

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/config/ProductionConfig.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/config/ProductionConfig.kt
@@ -3,20 +3,20 @@ package io.github.mfabisiak.darwinworld.config
 import io.github.mfabisiak.darwinworld.model.Position
 import kotlin.random.Random
 
-class ProductionConfig(
-    override val numberOfPlants: Int,
-    override val plantsGrowingEachDay: Int,
-    override val numberOfAnimals: Int,
-    override val energyConsumedEachDay: Int,
-    override val energyRequiredToBreed: Int,
-    override val energyGivenToNewborn: Int,
-    override val minNumberOfMutations: Int,
-    override val maxNumberOfMutations: Int,
-    override val genotypeSize: Int,
-    override val lowerBound: Position,
-    override val upperBound: Position,
-    override val energyFromSinglePlant: Int,
-    override val initialEnergy: Int
+data class ProductionConfig(
+    override val numberOfPlants: Int = 5,
+    override val plantsGrowingEachDay: Int = 3,
+    override val numberOfAnimals: Int = 4,
+    override val energyConsumedEachDay: Int = 10,
+    override val energyRequiredToBreed: Int = 20,
+    override val energyGivenToNewborn: Int = 10,
+    override val minNumberOfMutations: Int = 1,
+    override val maxNumberOfMutations: Int = 3,
+    override val genotypeSize: Int = 5,
+    override val lowerBound: Position = Position(0,0),
+    override val upperBound: Position = Position(5,5) ,
+    override val energyFromSinglePlant: Int = 20,
+    override val initialEnergy: Int = 100
 ) : SimulationConfig {
 
     override val jungle = let {
