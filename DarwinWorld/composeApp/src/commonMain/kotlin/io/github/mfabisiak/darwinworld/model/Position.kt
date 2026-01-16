0626dec78671e3124fff815c0509646feca0c0eb
Mateusz Fabisiak <mfabisiak@student.agh.edu.pl>
2026-01-16 04:15:31 +0100

    Add infix functions for over and under comparisons in Position class

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/Position.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/model/Position.kt
@@ -13,10 +13,10 @@ data class Position(val x: Int, val y: Int) {
 
     operator fun rangeTo(other: Position) = PositionClosedRange(this, other)
 
-    infix fun overOrUnder(range: PositionClosedRange) = this.y > range.end.y || this.y < range.start.y
+    infix fun over(range: PositionClosedRange) = this.y > range.end.y
 
-    infix fun exceedsOnSides(range: PositionClosedRange) = this.x < range.start.x || this.x > range.end.x
-
+    infix fun under(range: PositionClosedRange) = this.y < range.start.y
+    
 }
 
 fun Direction.movement(): Position = when(this) {
@@ -32,6 +32,12 @@ fun Direction.movement(): Position = when(this) {
 
 
 class PositionClosedRange(val start: Position, val end: Position) : Iterable<Position> {
+    val width
+        get() = end.x - start.x + 1
+
+    val height
+        get() = end.y - start.y + 1
+
     override fun iterator(): Iterator<Position> = Vector2dIterator(start, end)
 
     operator fun contains(position: Position) = position follows start && position precedes end
