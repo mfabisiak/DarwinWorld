a2155bdc5427a4779a053daada1a22ab73419bae
Mateusz Fabisiak <mfabisiak@student.agh.edu.pl>
2026-01-16 03:28:27 +0100

    Add grid lines to MapVisualizer for better visual reference

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/ui/components/MapVisualizer.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/ui/components/MapVisualizer.kt
@@ -1,46 +1,27 @@
 package io.github.mfabisiak.darwinworld.ui.components
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.drawscope.draw
 import androidx.compose.ui.graphics.drawscope.translate
-import androidx.compose.ui.input.key.Key.Companion.R
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
-import darwinworld.composeapp.generated.resources.Res
-import darwinworld.composeapp.generated.resources.animal_0
-import darwinworld.composeapp.generated.resources.animal_135
-import darwinworld.composeapp.generated.resources.animal_180
-import darwinworld.composeapp.generated.resources.animal_225
-import darwinworld.composeapp.generated.resources.animal_270
-import darwinworld.composeapp.generated.resources.animal_315
-import darwinworld.composeapp.generated.resources.animal_45
-import darwinworld.composeapp.generated.resources.animal_90
-import io.github.mfabisiak.darwinworld.model.Direction
-import io.github.mfabisiak.darwinworld.model.Position
+import darwinworld.composeapp.generated.resources.*
 import io.github.mfabisiak.darwinworld.model.map.WorldMap
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
-fun MapVisualizer(worldMap: WorldMap, height: Int, width: Int ){
+fun MapVisualizer(worldMap: WorldMap, height: Int, width: Int) {
 
-    val animalDrawables =  listOf(
+    val animalDrawables = listOf(
             painterResource(Res.drawable.animal_0),
             painterResource(Res.drawable.animal_45),
             painterResource(Res.drawable.animal_90),
@@ -58,12 +39,10 @@ fun MapVisualizer(worldMap: WorldMap, height: Int, width: Int ){
         contentAlignment = Alignment.Center
     ) {
         val cellSizeDp = min(maxWidth / width, maxHeight / height)
-
         val canvasWidth = cellSizeDp * width
         val canvasHeight = cellSizeDp * height
 
         Canvas(modifier = Modifier.size(canvasWidth, canvasHeight)) {
-
             val cellWidthPx = size.width / width
             val cellHeightPx = size.height / height
             val cellSizePx = Size(cellWidthPx, cellHeightPx)
@@ -71,10 +50,8 @@ fun MapVisualizer(worldMap: WorldMap, height: Int, width: Int ){
             drawRect(color = Color(0xFFF1F8E9))
 
             val jungleBounds = worldMap.config.jungle
-
             val jungleX = jungleBounds.start.x * cellWidthPx
             val jungleY = jungleBounds.start.y * cellHeightPx
-
             val jungleWidth = (jungleBounds.end.x - jungleBounds.start.x + 1) * cellWidthPx
             val jungleHeight = (jungleBounds.end.y - jungleBounds.start.y + 1) * cellHeightPx
 
@@ -84,6 +61,23 @@ fun MapVisualizer(worldMap: WorldMap, height: Int, width: Int ){
                 size = Size(jungleWidth, jungleHeight)
             )
 
+            for (i in 0..width) {
+                drawLine(
+                    Color.Gray,
+                    Offset(i * cellWidthPx, 0f),
+                    Offset(i * cellWidthPx, size.height),
+                    strokeWidth = 1f
+                )
+            }
+            for (i in 0..height) {
+                drawLine(
+                    Color.Gray,
+                    Offset(0f, i * cellHeightPx),
+                    Offset(size.width, i * cellHeightPx),
+                    strokeWidth = 1f
+                )
+            }
+
             for (plantPos in worldMap.plants) {
                 drawRect(
                     color = Color.Green,
@@ -91,6 +85,7 @@ fun MapVisualizer(worldMap: WorldMap, height: Int, width: Int ){
                     size = cellSizePx
                 )
             }
+
             for (animal in worldMap.animals.values) {
                 val x = animal.position.x * cellWidthPx
                 val y = animal.position.y * cellHeightPx
@@ -103,15 +98,7 @@ fun MapVisualizer(worldMap: WorldMap, height: Int, width: Int ){
                         draw(size = Size(cellWidthPx, cellHeightPx))
                     }
                 }
-                for (i in 0..width) {
-                    drawLine(Color.Gray, Offset(i * cellWidthPx, 0f), Offset(i * cellWidthPx, size.height))
-                }
-                for (i in 0..height) {
-                    drawLine(Color.Gray, Offset(0f, i * cellHeightPx), Offset(size.width, i * cellHeightPx))
-                }
             }
-
-
         }
     }
 }
