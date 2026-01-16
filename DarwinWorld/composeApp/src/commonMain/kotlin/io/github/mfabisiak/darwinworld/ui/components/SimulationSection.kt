ab68f97cac6e86ec4f79724540e02ed7ed49d40b
Maciej Bednarczyk <mbednarczy@student.agh.edu.pl>
2026-01-16 01:26:42 +0100

    Add different sections for config

---
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/ui/components/SimulationSection.kt
@@ -0,0 +1,37 @@
+package io.github.mfabisiak.darwinworld.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import io.github.mfabisiak.darwinworld.config.ProductionConfig
+
+
+@Composable
+fun SimulationSection(config: ProductionConfig, onConfigChange: (ProductionConfig) -> Unit) {
+    Column(
+        modifier = Modifier
+
+    ) {
+        Text(
+            "Symulacja",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+        ConfigInput("Liczba roślin:", config.numberOfPlants) {
+            onConfigChange(config.copy(numberOfPlants = it))
+        }
+
+        ConfigInput("Liczba roślin rosnących codziennie:", config.plantsGrowingEachDay) {
+            onConfigChange(config.copy(plantsGrowingEachDay = it))
+        }
+
+        ConfigInput("Liczba zwierząt:", config.numberOfAnimals) {
+            onConfigChange(config.copy(numberOfAnimals = it))
+        }
+    }
+}
+
