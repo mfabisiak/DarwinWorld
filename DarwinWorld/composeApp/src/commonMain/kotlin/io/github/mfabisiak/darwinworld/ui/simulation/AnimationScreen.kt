a517b7550c4fd2e54fbee99318e456c013575ead
Maciej Bednarczyk <mbednarczy@student.agh.edu.pl>
2026-01-23 12:58:29 +0100

    add statistics visualization in simulation

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/ui/simulation/AnimationScreen.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/ui/simulation/AnimationScreen.kt
@@ -1,11 +1,13 @@
 package io.github.mfabisiak.darwinworld.ui.simulation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import io.github.mfabisiak.darwinworld.logic.config.SimulationConfig
+import io.github.mfabisiak.darwinworld.statistics.CalculateStatistics
 import io.github.mfabisiak.darwinworld.statistics.createStatisticsSaver
 import io.github.mfabisiak.darwinworld.ui.simulation.components.MapVisualizer
 import io.github.mfabisiak.darwinworld.viewmodel.SimulationViewModel
@@ -21,11 +23,13 @@ fun AnimationScreen(config: SimulationConfig) {
 
     val simulationState by viewModel.simulationState.collectAsState()
 
+    val stats = CalculateStatistics(simulationState)
+
     LaunchedEffect(Unit) {
         viewModel.start()
     }
 
-    Column {
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
         Row {
             Button(enabled = !isRunning, onClick = { viewModel.previous() }) { Text("Poprzedni") }
             Button(onClick = {
@@ -36,7 +40,25 @@ fun AnimationScreen(config: SimulationConfig) {
             }
             Button(enabled = !isRunning, onClick = { viewModel.next() }) { Text("Następny") }
         }
-        MapVisualizer(simulationState.worldMap, config.upperBound.y + 1, config.upperBound.x + 1)
+        Row(modifier = Modifier.fillMaxSize()) {
+            Box(modifier = Modifier.weight(1f)) {
+                MapVisualizer(simulationState.worldMap, config.upperBound.y + 1, config.upperBound.x + 1)
+            }
+
+            Column(
+                modifier = Modifier
+                    .width(250.dp)
+            ) {
+                Text("Dzień: ${stats.currentDay}")
+                Text("Zwierzaki: ${stats.totalAnimals}")
+                Text("Rośliny: ${stats.totalPlants}")
+                Text("Wolne pola: ${stats.freeAreas}")
+                Text("Średnia Energia: ${stats.energy}")
+                Text("Średnia długość życia: ${stats.age}")
+                Text("Średnia ilość Dzieci: ${stats.children}")
+                Text("Genotypy: ${stats.popularGenotypes}")
+            }
+        }
     }
 
 }
