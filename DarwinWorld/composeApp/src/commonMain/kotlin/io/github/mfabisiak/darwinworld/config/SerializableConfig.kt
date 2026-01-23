2d061d34b81ffc74e9992ab7bea4a72e1f81ed3a
Mateusz Fabisiak <mfabisiak@student.agh.edu.pl>
2026-01-24 00:29:18 +0100

    Add configuration presets for simulation: classic, desert, full of plants, and fast animals

--- DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/config/SerializableConfig.kt
+++ DarwinWorld/composeApp/src/commonMain/kotlin/io/github/mfabisiak/darwinworld/config/SerializableConfig.kt
@@ -45,3 +45,107 @@ data class SerializableConfig(
         fastAnimalsEnabled
     )
 }
+
+val CLASSIC_CONFIG = SerializableConfig(
+    mapWidth = 20,
+    mapHeight = 20,
+    numberOfPlants = 40,
+    plantsGrowingEachDay = 5,
+    energyFromSinglePlant = 20,
+
+    numberOfAnimals = 15,
+    initialEnergy = 50,
+    energyConsumedEachDay = 1,
+
+    energyRequiredToBreed = 30,
+    energyGivenToNewborn = 15,
+
+    genotypeSize = 8,
+    minNumberOfMutations = 0,
+    maxNumberOfMutations = 2,
+
+    fastAnimalsEnabled = false,
+    energyRequiredToMoveFast = 5,
+    energyPerExtraStep = 5,
+    maxRange = 1,
+
+    randomSeed = 101
+)
+
+val DESERT_CONFIG = SerializableConfig(
+    mapWidth = 50,
+    mapHeight = 50,
+    numberOfPlants = 10,
+    plantsGrowingEachDay = 1,
+    energyFromSinglePlant = 100,
+
+    numberOfAnimals = 30,
+    initialEnergy = 150,
+    energyConsumedEachDay = 2,
+
+    energyRequiredToBreed = 200,
+    energyGivenToNewborn = 80,
+
+    genotypeSize = 10,
+    minNumberOfMutations = 0,
+    maxNumberOfMutations = 1,
+
+    fastAnimalsEnabled = true,
+    energyRequiredToMoveFast = 10,
+    energyPerExtraStep = 5,
+    maxRange = 3,
+
+    randomSeed = 202
+)
+
+val FULL_OF_PLANTS_CONFIG = SerializableConfig(
+    mapWidth = 15,
+    mapHeight = 15,
+    numberOfPlants = 100,
+    plantsGrowingEachDay = 30,
+    energyFromSinglePlant = 10,
+
+    numberOfAnimals = 50,
+    initialEnergy = 30,
+    energyConsumedEachDay = 1,
+
+    energyRequiredToBreed = 15,
+    energyGivenToNewborn = 8,
+
+    genotypeSize = 5,
+    minNumberOfMutations = 2,
+    maxNumberOfMutations = 6,
+
+    fastAnimalsEnabled = false,
+    energyRequiredToMoveFast = 0,
+    energyPerExtraStep = 0,
+    maxRange = 1,
+
+    randomSeed = 303
+)
+
+val FAST_ANIMALS_CONFIG = SerializableConfig(
+    mapWidth = 30,
+    mapHeight = 30,
+    numberOfPlants = 40,
+    plantsGrowingEachDay = 8,
+    energyFromSinglePlant = 25,
+
+    numberOfAnimals = 20,
+    initialEnergy = 70,
+    energyConsumedEachDay = 1,
+
+    energyRequiredToBreed = 50,
+    energyGivenToNewborn = 25,
+
+    genotypeSize = 20,
+    minNumberOfMutations = 5,
+    maxNumberOfMutations = 15,
+
+    fastAnimalsEnabled = true,
+    energyRequiredToMoveFast = 5,
+    energyPerExtraStep = 2,
+    maxRange = 5,
+
+    randomSeed = 404
+)
